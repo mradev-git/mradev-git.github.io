d91b25e9d6b1a4a46020b369beff8a8745b28551
vvoideye <vvoideye@users.noreply.github.com>
2020-06-15 19:28:45 +0700

    Adding main script installer
    After rewriting the code finally "easy install script" completed,

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#for whoever read this i create as simple as possible, my target is for termux
+#so i'll prefer whatever available options / program that present on default termux
+#thats why i dont use git, if u prefer git version, for transparancy reason maybe, please check on git bellow
+#all of the program used on this script is from multiple source, i'll drop them below
+#https://github.com/aztecrabbit/brainfuck-psiphon-pro-go
+#https://rureka.com/brainfuck-psiphon-pro-go-version-untuk-openwrt-linux-macos-dan-windows/
+
+
+echo "Welcome to simple setup brainF"
+echo " Requirement : Termux, Socksdroid/tun2tap/proxydroid(root)"
+echo "credit : aztecrabbit,rureka.com,woiden group"
+echo "For Sockdroid and others apps you can install it from Playstore"
+read -p "Press any key to start"
+mkdir Brainfuck
+cd Brainfuck
+echo "Downloading and extracting essential files"
+curl -o brf.zip http://mraa.me/brainfuck-psiphon-pro-go-arm.zip
+#curl -O 
+#chmod +x 
+#original server (rureka.com) delete hastag bellow
+#curl -o brf.zip http://rureka.com/download/brainfuck-psiphon-pro-go/brainfuck-psiphon-pro-go-linux-amd64.zip
+unzip -j -n -P rureka-psiphon brf.zip
+echo "SETUP COMPLETE"
+echo "Please run './startGamemax' next time to start brainfuck"
+read -p "Press any key to exit," 
+./brainfuck-psiphon-pro-go
+exit
