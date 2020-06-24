98b2b513b8cb3b141930659c6e232131f7ae1a1c
vvoideye <vvoideye@users.noreply.github.com>
2020-06-24 22:23:39 +0700

    Update config.sh
    Debugging again

--- config.sh
+++ config.sh
@@ -9,11 +9,11 @@ select opt in "${options[@]}"
 do
     case $opt in
         "[Telkomsel] Gamemax")
-            ./brf -f 118.98.95.120:443 -w akamai.net:443
+            ./Brainfuck/brf -f 118.98.95.120:443 -w akamai.net:443
             break
             ;;
         "[Indosat] Ilmupedia")
-            ./brf -f c.shared.global.fastly.net:443,rg-video.ruangguru.com:443 -w fastly.net:443
+            ./Brainfuck/brf -f c.shared.global.fastly.net:443,rg-video.ruangguru.com:443 -w fastly.net:443
             break
             ;;
         "[XL] Dirumah aja 3GB")
